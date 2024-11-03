dae74ed49f2d082de0f7b72e88372d443278ed09
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2024-11-03 19:37:51 +0400

    feat: add navigation

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,7 +1,44 @@
-<script setup></script>
+<script setup>
+const navigation = [
+  {
+    name: "ავტო სესხი",
+    link: "/auto-loan",
+  },
+  {
+    name: "ბიზნეს სესხი",
+    link: "/business-loan",
+  },
+  {
+    name: "თვითდასაქმებულთათვის",
+    link: "/self-employed",
+  },
+  {
+    name: "ჩვენს შესახებ",
+    link: "/about-us",
+  },
+  {
+    name: "ბლოგი",
+    link: "/blog",
+  },
+  {
+    name: "კონტაქტი",
+    link: "/contact",
+  },
+];
+</script>
 
 <template>
-  <h2>Navigation</h2>
+  <nav class="py-4 px-6 tablet:px-20 laptop:px-28 mx-auto w-fit">
+    <ul class="flex items-center justify-center gap-x-12 gap-y-5 flex-wrap">
+      <li v-for="navObj in navigation">
+        <NuxtLink
+          :to="navObj.link"
+          class="text-[#20232D] text-sm font-medium"
+          >{{ navObj.name }}</NuxtLink
+        >
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped></style>
